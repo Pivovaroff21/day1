f689035e00586856e0f0b60bd6a0d790509b0d0b
Pyvovarav laptop <bogdan21122003@gmail.com>
2023-01-13 21:47:39 +0200

    day 2 done

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require("path");
 const webpack = require("webpack");
+const { VueLoaderPlugin } = require("vue-loader");
+
 
 module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: "./app.js",
   resolve: {
     alias: {
       vue: "vue/dist/vue.esm-bundler",
@@ -11,12 +15,21 @@ module.exports = {
     static: path.join(__dirname, "src"),
     compress: true,
     port: 8080,
-    hot:true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
       __VUE_OPTIONS_API__: true,
       __VUE_PROD_DEVTOOLS__: false,
     }),
+    new VueLoaderPlugin()
   ],
 };
